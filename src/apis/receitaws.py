496d3db4da0d12c81b3b6aa5651f5da0155ebea2
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-21 14:45:03 -0300

    Fix response handling in cnpj_lookup function

--- src/apis/receitaws.py
+++ src/apis/receitaws.py
@@ -3,5 +3,5 @@ import requests
 
 def cnpj_lookup(cnpj: str):
     formatted_cnpj = cnpj.replace(".", "").replace("/", "").replace("-", "").strip()
-    response = requests.get(f"https://receitaws.com.br/v1/cnpj/{formatted_cnpj}")
-    return response.json()
+    response = requests.get(f"https://receitaws.com.br/v1/cnpj/{formatted_cnpj}").json()
+    return response
