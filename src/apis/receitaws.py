9de6441a663284de0ce3b418aa367d91bdc39b56
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-01-21 22:17:42 -0300

    Wrap cnpj_lookup function in try-except block for improved error handling

--- src/apis/receitaws.py
+++ src/apis/receitaws.py
@@ -3,28 +3,36 @@ import requests
 
 
 def cnpj_lookup(codcoligada: str, codcfo: str, cnpj: str, ie: str = ""):
-    formatted_cnpj = cnpj.replace(".", "").replace("/", "").replace("-", "").strip()
-    resp = requests.get(f"https://receitaws.com.br/v1/cnpj/{formatted_cnpj}").json()
+    try:
+        formatted_cnpj = cnpj.replace(".", "").replace("/", "").replace("-", "").strip()
+        
+        resp = requests.get(f"https://receitaws.com.br/v1/cnpj/{formatted_cnpj}", timeout=30).json()
+        
+        if isinstance(resp, dict) and resp.get("status") == "ERROR":
+            raise Exception
+        
+        response = {
+            "companyId": codcoligada,
+            "code": codcfo,
+            "shortName": suffix_remover(format_name(resp["fantasia"])) if resp["fantasia"] else suffix_remover(format_name(resp["nome"])),
+            "name": format_name(resp["nome"]),
+            "type": 3 if codcfo.upper().startswith('C') else 2,  # Conferir os tipos de cadastro
+            "mainNIF": resp["cnpj"].strip(),
+            "stateRegister": ie if ie and "isento" not in ie.lower() else "",
+            "zipCode": format_zipcode(resp["cep"]),
+            "streetType": format_street(resp["logradouro"])[0],
+            "streetName": format_street(resp["logradouro"])[1],
+            "number": resp["numero"].upper().strip(),
+            "districtType": format_district(resp["bairro"])[0],
+            "district": format_district(resp["bairro"])[1],
+            "stateCode": resp["uf"].upper().strip(),
+            "cityInternalId": "",  # Formatar
+            "phoneNumber": format_phone(resp["telefone"]),
+            "email": resp["email"].lower().strip(),
+            "contributor": 2 if ie and "isento" in ie.lower() else (1 if ie else 0)  # 0 = Não contribuinte | 1 = Contribuinte | 2 = Isento
+        }
+        
+        return response
     
-    response = {
-        "companyId": codcoligada,
-        "code": codcfo,
-        "shortName": suffix_remover(format_name(resp["fantasia"])) if resp["fantasia"] else suffix_remover(format_name(resp["nome"])),
-        "name": format_name(resp["nome"]),
-        "type": 3 if codcfo.upper().startswith('C') else 2,  # Conferir os tipos de cadastro
-        "mainNIF": resp["cnpj"].strip(),
-        "stateRegister": ie if ie and "isento" not in ie.lower() else "",
-        "zipCode": format_zipcode(resp["cep"]),
-        "streetType": format_street(resp["logradouro"])[0],
-        "streetName": format_street(resp["logradouro"])[1],
-        "number": resp["numero"].upper().strip(),
-        "districtType": format_district(resp["bairro"])[0],
-        "district": format_district(resp["bairro"])[1],
-        "stateCode": resp["uf"].upper().strip(),
-        "cityInternalId": "",  # Formatar
-        "phoneNumber": format_phone(resp["telefone"]),
-        "email": resp["email"].lower().strip(),
-        "contributor": 2 if ie and "isento" in ie.lower() else (1 if ie else 0)  # 0 = Não contribuinte | 1 = Contribuinte | 2 = Isento
-    }
-    
-    return response
+    except Exception as e:
+        print(f"exception: {resp.get('message')}")
